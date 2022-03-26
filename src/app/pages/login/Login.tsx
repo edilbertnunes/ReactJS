b0ff43a8404d88249a057d60a8a9f08b3fe257b0
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-03-26 15:35:43 -0300

    Aula6

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,16 +1,45 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
-    const history = useNavigate();
+    // o State serve para que os valores dos componentes sejam atualizados
+    // componete inicia com vazio
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
 
-    const handleClick = () => {
-        history('/inicial');
+    const handleEntrar = () => {
+        console.log(email);
+        console.log(password);
     }
 
+    // const history = useNavigate();
+
+    // const handleClick = () => {
+    //     history('/inicial');
+    // }
+
+    // return (
+    //     <div>
+    //         <p>Login</p>
+    //         <button onClick={handleClick}>Pagina Inicial</button>
+    //     </div>
+    // );
     return (
         <div>
-            <p>Login</p>
-            <button onClick={handleClick}>Pagina Inicial</button>
+            <form>
+                <label>
+                    <span>Email</span>
+                    <input value={email} onChange={e=>setEmail(e.target.value)}/>
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={password} onChange={e=>setPassword(e.target.value)}/>
+                </label>
+                
+                <button type="button" onClick={handleEntrar} /* tipo button para enviar o reload da pagina */>Entrar</button>
+            </form>
         </div>
     );
 }
+
