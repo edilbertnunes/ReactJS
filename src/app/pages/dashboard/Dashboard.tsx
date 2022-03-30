a3df7c965a31b05180ce591dd4e8f1e60345e668
Edilbert Nunes <edilbert.nunes@gmail.com>
2022-03-29 21:58:28 -0300

    Aulas 21 a 24

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,13 @@
 import { useCallback, useState } from "react";
 
+interface ITarefa {
+  id: number;
+  title: string;
+  isCompleted: boolean;
 
+}
 export const Dashboard = () => {
-  const [lista, setLista] = useState<string[]>(["Teste1","Teste2","Teste3"]);
+  const [lista, setLista] = useState<ITarefa[]>([]);
 
   const handleInputkeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback ((e) =>{
     if(e.key=== 'Enter') {
@@ -13,8 +18,15 @@ export const Dashboard = () => {
       e.currentTarget.value = ''; // limpa o input depois do enter
 
       setLista((oldLista)=> {
-        if (oldLista.includes(value)) return oldLista;
-        return [...oldLista,value];
+        if (oldLista.some((ListItem)=>ListItem.title===value)) return oldLista;
+        return [
+          ...oldLista,
+          {
+            id: oldLista.length,
+            title:value,
+            isCompleted: false
+          }
+          ];
       });
     }
   },[]);
@@ -26,9 +38,30 @@ export const Dashboard = () => {
       <input
       onKeyDown={handleInputkeyDown} />
 
+      <p>{lista.filter((ListItem)=>ListItem.isCompleted).length}</p>
+
       <ul>
-        {lista.map((value)=> {
-          return <li key={value}>{value}</li>;
+        {lista.map((ListItem,index) => {
+          return <li key={ListItem.id}>          
+            <input 
+            type="checkbox" 
+            checked={ListItem.isCompleted}
+            onChange={() => {
+              setLista(oldLista => {
+                return oldLista.map(oldListItem => {
+                  const newIsSelected = oldListItem.title === ListItem.title
+                    ? !oldListItem.isCompleted
+                    : oldListItem.isCompleted;
+                    return {
+                      ...oldListItem,
+                      isSelected: newIsSelected,
+                    };
+                });
+              });
+            }}
+        />
+            {ListItem.title}
+            </li>;
         })}
       </ul>
     </div>
